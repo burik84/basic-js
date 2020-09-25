0e29b9d859dd5292097a6abd0493c0e75d3aa32f
burik84 <avburik84@gmail.com>
2020-09-25 11:53:48 +0300

    feat: add season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason( date ) {
+  if (date instanceof Date && Object.getOwnPropertyNames(date).length === 0) {
+    const month = date.getMonth();
+    if (month > 1 && month < 5) {
+      return 'spring';
+    } else if (month > 4 && month < 8) {
+      return 'summer';
+    } else if (month > 7 && month < 11) {
+      return 'autumn';
+    } else {
+      return 'winter';
+    }
+  } else if (!date) {
+    return 'Unable to determine the time of year!';
+  } else {
+    throw new Error;
+  }
 };
